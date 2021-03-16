3c4e10d58e8509edf43834294001b09fc9e9fd3d
Galo Sandoval <galosan92@gmail.com>
2021-03-16 11:21:32 -0700

    adds logic to login component, creates dummyData file

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -1,11 +1,90 @@
 import $ from "jquery";
+import { useState } from "react";
+
 import { StyledLogin } from "./StyledLogin";
+const dummyData = require("../../DummyData.json");
+
+const initialMemberForm = {
+  username: "",
+  password: "",
+};
+
+const initialCompanyForm = {
+  companyUsername: "",
+  password: "",
+};
 
 export const Login = () => {
+  const [currentUser, setCurrentUser] = useState("");
+  const [memberFormValues, setMemberFormValues] = useState(initialMemberForm);
+  const [companyFormValues, setCompanyFormValues] = useState(
+    initialCompanyForm
+  );
 
+  const handleChange = (e) => {
+    if (e.target.placeholder === "Company Username") {
+      setCompanyFormValues({
+        ...companyFormValues,
+        companyUsername: e.target.value,
+      });
+    }
+    if (e.target.id === "company-password") {
+      setCompanyFormValues({ ...companyFormValues, password: e.target.value });
+    }
+    if (e.target.placeholder === "Username") {
+      setMemberFormValues({ ...memberFormValues, username: e.target.value });
+    }
+    if (e.target.id === "member-password") {
+      setMemberFormValues({ ...memberFormValues, password: e.target.value });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (e.target.role.value === "company") {
+      if (
+        dummyData.data[0].user.filter(
+          (user) => user.username === companyFormValues.companyUsername
+        ) &&
+        dummyData.data[0].user.filter(
+          (user) => user.password === companyFormValues.password
+        )
+      ) {
+        console.log("hi");
+
+        setCurrentUser(companyFormValues.companyUsername);
+        console.log(currentUser);
+      } else if (e.target.role.value === "member") {
+        if (
+          dummyData.data[0].user.filter(
+            (user) => user.username === memberFormValues.username
+          ) &&
+          dummyData.data[0].user.filter(
+            (user) => user.password === memberFormValues.password
+          )
+        ) {
+          console.log("hi");
+          setCurrentUser(memberFormValues.username);
+          console.log(currentUser);
+        }
+      }
+    }
+  };
   const handleSelect = (e) => {
+    if (e.target.value === "default") {
+      $(".login-container form .member-select").addClass("hide");
+      $(".login-container form .company-select").addClass("hide");
+      $(".button").addClass("hide");
+    }
     if (e.target.value === "company") {
+      $(".login-container form .member-select").addClass("hide");
       $(".login-container form .company-select").toggleClass("hide");
+      $(".button").removeClass("hide");
+    }
+    if (e.target.value === "member") {
+      $(".login-container form .company-select").addClass("hide");
+      $(".login-container form .member-select").toggleClass("hide");
+      $(".button").removeClass("hide");
     }
   };
 
@@ -13,22 +92,44 @@ export const Login = () => {
     <StyledLogin className="login">
       <div className="login-container">
         <h1>Login</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <select onChange={handleSelect} name="role" id="role">
             <option value="default">Choose your role</option>
             <option value="company">Company</option>
             <option value="member">Team Member</option>
           </select>
           <div className="member-select hide">
-            <input placeholder="First Name" type="text" />
-            <input placeholder="Last Name" type="text" />
-            <input placeholder="Username" type="text" />
-            <input placeholder="Password" type="password" />
+            <input
+              onChange={handleChange}
+              value={memberFormValues.username}
+              placeholder="Username"
+              type="text"
+            />
+            <input
+              id="member-password"
+              onChange={handleChange}
+              value={memberFormValues.password}
+              placeholder="Password"
+              type="password"
+            />
           </div>
           <div className="company-select hide">
-            <input placeholder="Company Name" type="text" />
-            <input placeholder="Description of your company..." type="text" />
-            <input placeholder="Password" type="password" />
+            <input
+              onChange={handleChange}
+              value={companyFormValues.companyUsername}
+              placeholder="Company Username"
+              type="text"
+            />
+            <input
+              id="company-password"
+              onChange={handleChange}
+              value={companyFormValues.password}
+              placeholder="Password"
+              type="password"
+            />
+          </div>
+          <div className="button hide">
+            <button type="submit">Login</button>
           </div>
         </form>
       </div>
