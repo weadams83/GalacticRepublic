56d1683ec3694020a10886d97750214ca319ae6a
Christopher DeWald <dewald1838@gmail.com>
2021-03-17 12:04:50 -0400

    navbar placeholder for search

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
             <a href = 'users'>Users</a>
             <a href = 'projects'>Projects</a>
             <a href = 'teams'>Teams</a>
-            <input type = "text" label="Search"/>
+            <input type = "text" placeholder="Search"/>
         </NavbarStyles>
     </div>
     )
