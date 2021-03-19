8015019b67a738aa4d0bb772fb14917ccc8a21b5
Nathan Breedlove <nathanielbreedlove@gmail.com>
2021-03-19 10:30:04 -0400

    fix stupid error

--- server/src/main/java/com/cooksys/server/controllers/UserController.java
+++ server/src/main/java/com/cooksys/server/controllers/UserController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 
 import com.cooksys.server.DTOs.CompanyResponseDTO;
@@ -72,5 +75,4 @@ public class UserController {
 	public UserResponseDTO deleteUser(@PathVariable("username") String userName, @RequestBody UserSignInRequestDTO userRequest) {
 		return userServ.deleteUser(userName, userRequest);
 	}
-
 }
