bbabdaac5e7d39903692c828374d397e98e2b18a
Galo Sandoval <galosan92@gmail.com>
2021-03-22 15:24:44 -0700

    adds redux store to save sign in details

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { loginReducer } from "./store/loginReducer";
+
+const store = createStore(
+  loginReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
