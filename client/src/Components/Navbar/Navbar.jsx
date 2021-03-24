7caa3565d36d027a88384fd96b3d01a69a14fecc
jRichardson36 <jefferson.richardson36@gmail.com>
2021-03-24 11:43:50 -0500

    styled the NavBar

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { removeUser } from "../../store/loginReducer";
 import { NavbarStyles } from "./StyledNavbar";
+import GR from '../Navbar/GR.png'
 
 const Navbar = () => {
   const dispatch = useDispatch();
@@ -11,6 +12,8 @@ const Navbar = () => {
   return (
     <div>
       <NavbarStyles>
+        <img src={GR} alt="Logo" width='40' height='30'/>
+        
         <Link to="/users">Users</Link>
         <Link to="/projects">Projects</Link>
         <Link to="/company">Teams</Link>
